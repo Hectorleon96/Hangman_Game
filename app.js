fa183e6872927f56c7d15eb1887e2db356b0c2d0
estarles <jobleon.hector@gmail.com>
2023-03-28 23:18:09 -0600

    complete functional game

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 import {
   USER_CHOOSE,
-  MAIN_GAME_CONTENT,
   MAIN_GAME_CHOOSE,
   SHOW_WORDS,
   GO_BUTTON,
   GAME_BUTTONS,
   ALERT_TEXT,
   WRONG_CHARACTER,
+  WIN,
 } from "./getElement.js";
 addEventListener("DOMContentLoaded", () => {
   let word = [];
-  let found = {};
+  let found;
   let lifes = 6;
   async function getWord() {
     let response = await fetch(
-      "https://clientes.api.greenborn.com.ar/public-random-word?c=1&l=8"
+      "https://clientes.api.greenborn.com.ar/public-random-word?c=1&l=6"
     );
 
     if (response.status !== 200) {
-      errorHandle("error", "Sorry! Something was wrong, try again...");
+      gameStatus("error", "Sorry! Something was wrong, try again...");
       return;
     }
     document.querySelector(".span-start").classList.add("hide");
     let data = await response.json();
+    found = data;
     word = data[0].split("");
     MAIN_GAME_CHOOSE.classList.remove("hide");
     GO_BUTTON.classList.add("hide");
@@ -36,49 +37,41 @@ addEventListener("DOMContentLoaded", () => {
       listItems.textContent = "_";
       SHOW_WORDS.appendChild(listItems);
     });
-
-    console.log(word);
   }
 
   function checkWord() {
     if (USER_CHOOSE.value === "") {
-      errorHandle("error", "Please type any character for continue...");
+      gameStatus("error", "Please type any character for continue...");
       return;
     }
 
     if (!word.includes(USER_CHOOSE.value)) {
       lifes = lifes - 1;
       if (lifes === 0) {
-        gameOver();
+        gameStatus("gameOver", `¡You lost! The word is: ' ${found}' `);
         USER_CHOOSE.value = "";
         return;
       }
-      errorHandle("wrongWord", `Wrong character, you have ${lifes} intents...`);
+      gameStatus("wrongWord", `Wrong character, you have ${lifes} intents...`);
       USER_CHOOSE.value = "";
       return;
     }
-
-    /*   word.forEach((element) => {
-      let position = word.indexOf(element);
-      found = { character: element, position: position };
-      word.splice(position, 1, "_");
-      renderWord(found.character, found.position);
-    }); */
-
-    console.log(found);
-    console.log(word);
+    showCharacter(USER_CHOOSE.value);
   }
 
-  /*  function renderWord(character, position) {
+  function showCharacter(character) {
+    let position = word.indexOf(character);
+    word.splice(position, 1, "_");
+    USER_CHOOSE.value = "";
     let render = document.querySelectorAll(".listItems");
     render[position].textContent = character;
-  } */
 
-  function gameOver() {
-    WRONG_CHARACTER.textContent = "";
-    SHOW_WORDS.innerHTML = `¡You lost! The word is: '${word.join("")}'`;
-    GAME_BUTTONS[1].classList.add("hide");
-    MAIN_GAME_CHOOSE.classList.add("hide");
+    let checkWin = word.every(function (element) {
+      return element === "_";
+    });
+    if (checkWin) {
+      gameStatus("youWin", "Press restart and play again c:");
+    }
   }
 
   function restartGame() {
@@ -86,27 +79,37 @@ addEventListener("DOMContentLoaded", () => {
     USER_CHOOSE.value = "";
     SHOW_WORDS.textContent = "";
     WRONG_CHARACTER.textContent = "";
+    WIN.classList.add("hide");
     getWord();
   }
 
-  function errorHandle(type, message) {
+  function gameStatus(type, message) {
     switch (type) {
       case "error":
         ALERT_TEXT.textContent = message;
-        errorDelay(ALERT_TEXT);
+        setTimeout(() => {
+          ALERT_TEXT.textContent = "";
+        }, 2000);
         break;
 
       case "wrongWord":
         WRONG_CHARACTER.textContent = message;
-        //errorDelay(WRONG_CHARACTER);
+        break;
+
+      case "gameOver":
+        WRONG_CHARACTER.textContent = "";
+        SHOW_WORDS.innerHTML = message;
+        GAME_BUTTONS[1].classList.add("hide");
+        MAIN_GAME_CHOOSE.classList.add("hide");
+        break;
+
+      case "youWin":
+        WIN.classList.remove("hide");
+        WRONG_CHARACTER.textContent = message;
+        MAIN_GAME_CHOOSE.classList.add("hide");
+        GAME_BUTTONS[1].classList.add("hide");
         break;
     }
-  }
-
-  function errorDelay(element) {
-    setTimeout(() => {
-      element.textContent = "";
-    }, 2000);
   }
 
   GAME_BUTTONS[0].addEventListener("click", restartGame);
